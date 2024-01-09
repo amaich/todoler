688244f0e2ed86398377a0cff97dfe44119feae4
AmaichMacOS <amaev773@mail.ru>
2024-01-09 23:39:43 +0300

    views added

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'todo'
+
 urlpatterns = [
     path("", views.hello_world),
-    path("todos/", views.TodoList.as_view())
+    path("todos/", views.TodoList.as_view(), name='todo_list'),
+    path("todos/create", views.TodoCreate.as_view(), name='todo_create'),
+    path("todos/<int:pk>", views.TodoDetail.as_view(), name='todo_detail'),
+    path("todos/<int:pk>/update", views.TodoUpdate.as_view(), name='todo_update'),
+    path("todos/<int:pk>/delete", views.TodoDelete.as_view(), name='todo_delete')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import View
+from django.urls import reverse_lazy, reverse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import TodoModel
 
@@ -10,9 +14,37 @@ def hello_world(request):
     return HttpResponse(todos)
 
 
-class TodoList(View):
+class TodoList(ListView):
+    model = TodoModel
+    template_name = 'todo/todo_list.html'
+    context_object_name = 'todos'
 
-    def get(self, request):
-        context = {'todos': TodoModel.objects.all()}
-        return render(request, 'todo/todo_list.html', context=context)
+
+class TodoCreate(CreateView):
+    model = TodoModel
+    template_name = 'todo/todo_create.html'
+    fields = ['name']
+    success_url = reverse_lazy('todo:todo_list')
+
+
+class TodoDetail(DetailView):
+    model = TodoModel
+    template_name = 'todo/todo_details.html'
+    context_object_name = 'todo'
+
+
+class TodoUpdate(UpdateView):
+    model = TodoModel
+    template_name = 'todo/todo_update.html'
+    context_object_name = 'todo'
+
+    def get_success_url(self):
+        return reverse('todo:todo_detail', kwargs={'pk': self.object.id})
+
+
+class TodoDelete(DeleteView):
+    model = TodoModel
+    template_name = 'todo/todo_delete.html'
+    context_object_name = 'todo'
+    success_url = reverse_lazy('todo:todo_delete')
 
